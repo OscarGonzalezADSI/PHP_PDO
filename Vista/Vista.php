4a4025bee947d421fac00f6d6492419b32477531
OSCAR GONZALEZ <55475821+OscarGonzalezADSI@users.noreply.github.com>
2020-09-01 05:39:43 -0500

    Add files via upload

---
+++ Vista/Vista.php
@@ -0,0 +1,24 @@
+<?php
+
+include "../Connection/PDOSelectSTMT.php";
+
+?>
+<table style='border: solid 1px black;'>
+<tr>
+	<th>Id</th>
+	<th>Firstname</th>
+	<th>Lastname</th>
+</tr>
+<?php
+$dbname = "myDBPDO";
+$SqlSelect = "SELECT * FROM MyGuests";
+try {
+  $origen = new PDOSelectSTMT();
+  $origen -> action($dbname, $SqlSelect);
+} catch(PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+$origen = null;
+
+?>
+</table>
